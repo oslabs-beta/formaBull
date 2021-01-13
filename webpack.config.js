114dc9503c1cbdc0a59e990783fd9a6871044d68
Max Nikitin <62187345+nikitinmax2300@users.noreply.github.com>
2021-01-12 21:37:40 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
+
+
