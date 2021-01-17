3c80078c2803d96e5f1bf924f9ada709d921ac82
Mario Eldin <marioeldin@icloud.com>
2021-01-16 16:31:49 -0800

    Created style.scss, refactored webpack.react.config.js, installed devDependencies, and imported style.scss into App.tsx

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -22,6 +22,10 @@ module.exports = {
         presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript" ],
       },
     },
+    {
+      test: /\.s(a|c)ss$/,
+      use: ["style-loader", "css-loader", "sass-loader"]    
+    },
     ]
   },
   devServer: {
