6e6f0d76e17fcf0364fc4dbe73dcceb9c8c1179c
Courtney <777.catalyst@gmail.com>
2021-01-20 16:02:48 -0600

    Card component drag functional

--- client/components/Cards.tsx
+++ client/components/Cards.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap';
+import { useDrag } from 'react-dnd';
+import { ItemTypes } from '../utils/items';
 
 
 
 export default function CardCreator(props:any) {
+     //useDrag returns an array of two things: an object that contains extra props(received from collecting functions), and a ref that binds and tells react-dnd that the result of this hook will be attached to the specific component
+  const[{ isDragging }, drag] = useDrag({
+    //takes two things: item and that item's type
+    //tells useDrag what item will be passed on
+    item: {
+      //Must specify type of item that will be dragged, this means only this //specific type of item will be allowed to be dragged into the MainCanvas
+      type: ItemTypes.CARD,
+      //when we have multiple draggable cards/components
+      //this will pass the id of the card while it is being dragged
+      // id: props._id,
+    }, 
+    //collect function - this is basically a way to transform state from the drag-and-drop system into usable props for the items/components
+    collect: monitor => ({
+        //monitor will return isDragging prop if monitor.isDragging is true and it is only true when the specific item is being dragged
+        isDragging: !!monitor.isDragging(),
+    })
+});
 
     return (
-        <Card style={{border: '1px solid black'}}>
+        <Card 
+        ref={drag} 
+        style={{opacity: isDragging ? 0.5 : 1, width: '18rem', border: '1px solid black'}}>
         <Card.Body>
             <Card.Title>Test</Card.Title>
             <Card.Text>
--- client/components/MainCanvas.tsx
+++ client/components/MainCanvas.tsx
@@ -2,59 +2,14 @@ import React from 'react';
 import DefaultForm from './DefaultForm';
 
 export default function MainCanvas (props:any) {
-    //drop event
-    const drop = (el:any) => {
-      //cancels the event if it is cancable, meaning that the default action that belongs to the event will not occur.
-      el.preventDefault();
-      //get form attribute(item to be dropped), pass in a "key" to getData, make sure to use the same when setting the data
-      const formButton_id = el.dataTransfer.getData('formButton_id')
-      const button = document.getElementById(formButton_id);
-    }
-    //turn canvas into droppable target by using useDrop hook
-      //returns addedProps drop will be used as a ref
-    // const[{addedProps}, drop] = useDrop({
-    //   //need to specify what type of item to accept
-    //   accept: 
-    // });
-    // //drop event
-    // const drop = el => {
-    //   //cancels the event if it is cancable, meaning that the default action that belongs to the event will not occur.
-    //   el.preventDefault();
-    //   //get form attribute(item to be dropped), pass in a "key" to getData, make sure to use the same when setting the data
-    //   const formButton_id = el.dataTransfer.getData('formButton_id')
-    //   const button = document.getElementById(formButton_id);
+ 
 
-    //   //hide the button when first tracking to give appearance of picking up the button
-    //   button.style.display = 'block';
-
-    //drag over event
-    const dragOver = (el:any) => {
-      //cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-      el.preventDefault()
-
-    // //drag over event
-    // const dragOver = el => {
-    //   //cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-    //   el.preventDefault()
-
-    }
-
-
-    return (
+      return (
         <div 
           //attaches the drop ref to the MainCanvas
-        //   ref={drop}
+          //   ref={drop}
           id={props.id}
           className={'main-canvas'}
-        //   //called when the button is dropped onto the canvas
-        //   onDrop={drop}
-        //   //called when one of the buttons is dragged over canvas
-        //   onDragOver={dragOver}
-        //   className='main-canvas'
-        //   //called when the button is dropped onto the canvas
-        //   onDrop={drop}
-        //   //called when one of the buttons is dragged over canvas
-        //   onDragOver={dragOver}
         >
           <DefaultForm />
           {/* allows anything that we pass into the Canvas component to be a child of the canvas component */}
