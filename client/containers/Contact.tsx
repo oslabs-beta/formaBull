275719d41c64c9bb045be4285cef1668262eb93d
Mario Eldin <marioeldin@icloud.com>
2021-01-20 13:52:19 -0800

    Connect react router to 5 new containters & refactor MenuDrawer and NavBar

---
+++ client/containers/Contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact () {
+
+  return(
+    <div className = 'contact'>
+      <h1 style={{textAlign: "center", paddingBottom: '400px'}} >Contact</h1>
+    </div>
+  )
+}
