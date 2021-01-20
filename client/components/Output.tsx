c4e71073a438214bc15178d4b2a5c0e6f6e4ddf5
Max Nikitin <teachandtravelcn@gmail.com>
2021-01-19 21:57:47 -0500

    fixed minor bugs

--- client/components/Output.tsx
+++ client/components/Output.tsx
@@ -2,36 +2,55 @@ import React from 'react';
 import DefaultForm from './DefaultForm';
 import { CopyBlock, dracula, nord, monokai } from "react-code-blocks";
 import ScrollToBottom from 'react-scroll-to-bottom';
+import TestRenderer from 'react-test-renderer';
+import Navbar from './NavBar';
+// console.log(JSON.stringify({DefaultForm}))
+const sampleJSX = `import React from "react";
+import { useForm } from "react-hook-form";
 
-// const example = 
+type Inputs = {
+  example: string,
+  exampleRequired: string,
+};
+
+export default function DefaultForm() {
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")) // watch input value by passing the name of it
+
+  return (
+    <div className = 'default-form'>
+    {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+    <form onSubmit={handleSubmit(onSubmit)}>
+    {/* register your input into the hook by invoking the "register" function */}
+    <label>Name</label>
+      <input name="example" defaultValue="test" ref={register} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <label>Last Name</label>
+      <input name="exampleRequired" ref={register({ required: true })} />
+      {/* errors will return when field validation fails  */}
+      <label>Age</label>
+      <input name="exampleRequired" ref={register({ required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>
+    </div>
+  );
+}
+`
 
 export default function Output() {
   return (
     <ScrollToBottom>
     <div className = 'output'>
       <CopyBlock 
-      text={`const fizzbuzzbazz = num => {
-        const results = [];
-        
-        for (let i = 1; i <= num; i++) {
-          let str = '';
-          if (i % 3 === 0)
-            str += 'fizz';
-          if (i % 5 === 0)
-            str += 'buzz';
-          if (i % 7 === 0)
-            str += 'bazz';
-          
-          results.push(str || i);
-        }
-        
-        return results;
-      };`}
-
-        // text={DefaultForm}
-        showLineNumbers
+        text={sampleJSX}
+        showLineNumbers={true}
         codeBlock
-        language="tsx"
+        language="typescript"
         theme={dracula}
       />
     </div>
