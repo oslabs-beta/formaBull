787c675885b032926b6b5e03f6bd7d52ffce66c4
Mario Eldin <marioeldin@icloud.com>
2021-01-20 19:35:33 -0800

    resolve merge conflicts

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -41,7 +41,6 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import clsx from 'clsx';
-import Routes from '../../routes/Routes';
 
 
 
