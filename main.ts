97f2f2634b1f1dacdf241f19b74444d9ecb3d153
Mario Eldin <marioeldin@icloud.com>
2021-01-28 10:58:06 -0800

    Merge commit

--- main.ts
+++ main.ts
@@ -1,11 +1,3 @@
-/**
- 
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-
-Or 
-
-*/
-// import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 // JS way, destructure
 const { app, BrowserWindow } = require('electron');
 
@@ -32,10 +24,3 @@ app.on('ready', () => {
   window.loadURL(`http://localhost:4000`); // development 
   window.webContents.openDevTools(); 
 });
-
-
-// app.whenReady().then(() => {
-//   installExtension(REACT_DEVELOPER_TOOLS)
-//       .then((name) => console.log(`Added Extension:  ${name}`))
-//       .catch((err) => console.log('An error occurred: ', err));
-// }); 
